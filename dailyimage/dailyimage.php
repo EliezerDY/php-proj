99cd827a2ebe8e6d2e1dafa888f53777c162240a
EliezerDY <42982224+EliezerDY@users.noreply.github.com>
2019-07-11 14:27:35 -0400

    Update dailyimage.php

--- dailyimage/dailyimage.php
+++ dailyimage/dailyimage.php
@@ -1,49 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <title>Days With Image</title>
+<body>
 <?php
 //display different image per day
 
 // Find what today is using date function
-$today=date(l);
+$today=date("l");
 
-if($today==Monday){
+if($today=="Monday"){
 echo "Today is Monday";
 echo "<BR>";
 echo "<img src='image1.gif'>";
 }
 
-elseif($today==Tuesday){
+elseif($today=="Tuesday"){
 echo "Today is Tuesday";
 echo "<BR>";
 echo "<img src='image2.gif'>";
 }
 
-elseif($today==Wednesday){
+elseif($today=="Wednesday"){
 echo "Today is Wednesday";
 echo "<BR>";
 echo "<img src='image3.gif'>";
 }
 
-elseif($today==Thursday){
+elseif($today=="Thursday"){
 echo "Today is Thursday";
 echo "<BR>";
 echo "<img src='image4.gif'>";
 }
 
-elseif($today==Friday){
+elseif($today=="Friday"){
 echo "Today is Friday";
 echo "<BR>";
 echo "<img src='image5.gif'>";
 }
 
-elseif($today==Saturday){
+elseif($today=="Saturday"){
 echo "Today is Saturday";
 echo "<BR>";
 echo "<img src='image6.gif'>";
 }
 
-elseif($today==Sunday){
+elseif($today=="Sunday"){
 echo "Today is Sunday";
 echo "<BR>";
 echo "<img src='image7.gif'>";
 }
 
 ?>
+</body>
+</html>
